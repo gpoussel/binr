b48c861210fd563758962b554d1428682abd4aec
Guillaume Poussel <gpoussel@gmail.com>
2019-01-19 16:28:36 +0100

    Start implementing lexer.

--- packages/configuration-parser/lib/configuration-parser.js
+++ packages/configuration-parser/lib/configuration-parser.js
@@ -10,21 +10,121 @@ function ConfigurationParser() {
 function buildParser() {
   const { createToken, Lexer } = chevrotain;
 
-  const headerToken = createToken({
-    name: "header",
-    pattern: /#define/,
-  });
-
-  const whitespaceToken = createToken({
+  const WhitespaceToken = createToken({
     name: "whitespace",
-    pattern: /\s+/,
+    pattern: /[ \t\r\n]+/,
     group: Lexer.SKIPPED,
   });
 
-  const allTokens = [headerToken, whitespaceToken];
+  const SingleLineCommentToken = createToken({
+    name: "Single-Line Comment",
+    pattern: /\/\/.+/,
+    group: "comments",
+  });
+
+  const MultiLineCommentToken = createToken({
+    name: "Multi-Line Comment",
+    pattern: /\/\*[\s\S]+\*\//,
+    group: "comments",
+  });
+
+  const IdentifierToken = createToken({
+    name: "Identifier",
+    pattern: /[a-z_][a-z0-9_]+/i,
+  });
+
+  const StringLiteralToken = createToken({
+    name: "StringLiteral",
+    pattern: /"(:?[^\\"\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/,
+  });
+
+  const NumberLiteralToken = createToken({
+    name: "NumberLiteral",
+    pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
+  });
+
+  const SemiColonToken = createToken({
+    pattern: /;/,
+    name: "SemiColon",
+  });
+
+  const ColonToken = createToken({
+    pattern: /:/,
+    name: "Colon",
+  });
+
+  const DirectiveStartToken = createToken({
+    pattern: /#/,
+    name: "DirectiveStart",
+  });
+
+  const EqualsToken = createToken({
+    pattern: /=/,
+    name: "Equals",
+  });
+
+  const CurlyBraceOpenToken = createToken({
+    pattern: /{/,
+    name: "CurlyBraceOpen",
+  });
+
+  const CurlyBraceCloseToken = createToken({
+    pattern: /}/,
+    name: "CurlyBraceClose",
+  });
+
+  const TrueToken = createToken({
+    pattern: /true/,
+    name: "True",
+  });
+
+  const FalseToken = createToken({
+    pattern: /false/,
+    name: "False",
+  });
+
+  const StructToken = createToken({
+    pattern: /struct/,
+    name: "Struct",
+  });
+
+  const ExportToken = createToken({
+    pattern: /export/,
+    name: "Export",
+  });
+
+  const allTokens = [
+    // Whitepsaces
+    WhitespaceToken,
+
+    // Comments
+    SingleLineCommentToken,
+    MultiLineCommentToken,
+
+    // Symbols
+    SemiColonToken,
+    ColonToken,
+    DirectiveStartToken,
+    EqualsToken,
+    CurlyBraceOpenToken,
+    CurlyBraceCloseToken,
+
+    // Keywords
+    TrueToken,
+    FalseToken,
+    StructToken,
+    ExportToken,
+
+    // Identifiers
+    IdentifierToken,
+    StringLiteralToken,
+    NumberLiteralToken,
+  ];
 
   return {
-    lexer: new Lexer(allTokens),
+    lexer: new Lexer(allTokens, {
+      ensureOptimizations: true,
+    }),
   };
 }
 
@@ -34,7 +134,11 @@ ConfigurationParser.prototype.parse = input => {
   }
 
   const parser = buildParser();
-  console.log(parser.lexer.tokenize(input));
+  const tokenizeResult = parser.lexer.tokenize(input);
+  if (!_.isEmpty(tokenizeResult.errors)) {
+    throw new Error(`Got an error while parsing input: ${_.first(tokenizeResult.errors).message}`);
+  }
+  console.log(tokenizeResult);
 
   return undefined;
 };
