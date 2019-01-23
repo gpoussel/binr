05f024c48ae066ddeafb60ebb21e56899fad427b
Guillaume Poussel <gpoussel@gmail.com>
2019-01-23 21:43:02 +0100

    Improve GIF unit test.
    Add the source link, and add a TODO item for bit fields (not supported yet).

--- packages/binary-reader/__tests__/binary-reader.test.js
+++ packages/binary-reader/__tests__/binary-reader.test.js
@@ -12,12 +12,17 @@ const reader = new BinaryReader();
 const gifDefinition = getGifDefinition();
 
 function getGifDefinition() {
+  // Source: https://www.sweetscape.com/010editor/repository/files/GIF.bt
+
   const logicalScreenStructure = new Structure("logical_screen", [
     new Field("imageWidth", new UintType(16)),
     new Field("imageHeight", new UintType(16)),
+    new Field("flags", new UintType(8)), // TODO: Use bit fields
+    new Field("backgroundColorIndex", new UintType(8)),
+    new Field("pixelAspectRatio", new UintType(8)),
   ]);
   const headerStructure = new Structure("header", [
-    new Field("magic", new StringType(3)),
+    new Field("signature", new StringType(3)),
     new Field("version", new StringType(3)),
   ]);
   const gifFileStructure = new Structure("gif_file", [
@@ -49,10 +54,13 @@ describe("BinaryReader", () => {
     const gifValue = reader.read(treeBuffer, gifDefinition, "gif_file");
     expect(gifValue).toBeDefined();
     expect(gifValue.header).toBeDefined();
-    expect(gifValue.header.magic).toBe("GIF");
+    expect(gifValue.header.signature).toBe("GIF");
     expect(gifValue.header.version).toBe("89a");
     expect(gifValue.logicalScreen).toBeDefined();
     expect(gifValue.logicalScreen.imageWidth).toBe(150);
     expect(gifValue.logicalScreen.imageHeight).toBe(189);
+    expect(gifValue.logicalScreen.flags).toBeDefined();
+    expect(gifValue.logicalScreen.backgroundColorIndex).toBe(1);
+    expect(gifValue.logicalScreen.pixelAspectRatio).toBe(0);
   });
 });
