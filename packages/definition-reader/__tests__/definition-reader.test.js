8f3ddf2110b9b8d2af02baf30e0c243b912e1b9a
Guillaume Poussel <gpoussel@gmail.com>
2019-01-30 13:10:07 +0100

    Add BMP sample definition as a fixture for unit tests.

--- packages/definition-reader/__tests__/definition-reader.test.js
+++ packages/definition-reader/__tests__/definition-reader.test.js
@@ -118,7 +118,7 @@ describe("DefinitionReader", () => {
   // Note: these definitions are not complete at all. They are often inspired by
   // a real-world format but they are never fully implemented.
   test(`parses example format definition`, () => {
-    _.each(["gif.binr", "class.binr"], filename => {
+    _.each(["gif.binr", "class.binr", "bmp.binr"], filename => {
       const structure = fs.readFileSync(`${pathToFixtures}/${filename}`, "utf-8");
       const result = createAndCallParser(structure)();
       expect(result).toBeInstanceOf(Definition);
