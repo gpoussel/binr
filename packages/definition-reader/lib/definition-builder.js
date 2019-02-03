a41cb57cc04893642643f418c6ac6c48b5280006
Guillaume Poussel <gpoussel@gmail.com>
2019-02-03 09:29:41 +0100

    Update DefinitionBuilder to cache all buildXXX() calls.
    While building a definition, elements (like structures, bitmasks and
    enumerations) will only be built once.

--- packages/definition-reader/lib/definition-builder.js
+++ packages/definition-reader/lib/definition-builder.js
@@ -19,21 +19,82 @@ class DefinitionBuilder {
   }
 
   build(ast) {
-    const enumerations = _.map(ast.enumerations, e => this.buildEnumeration(ast, e));
-    const bitmasks = _.map(ast.bitmasks, e => this.buildBitmask(ast, e));
+    const enumerations = _.keyBy(_.map(ast.enumerations, e => this.buildEnumeration(e)), "name");
+    const bitmasks = _.keyBy(_.map(ast.bitmasks, e => this.buildBitmask(e)), "name");
     const builtElements = {
       enumerations,
       bitmasks,
     };
-    const structures = _.map(ast.structures, s => this.buildStructure(ast, builtElements, s));
+    const structures = _.values(this.buildAllStructures(ast, ast.structures, builtElements));
     return new Definition(structures, enumerations, bitmasks);
+  }
+
+  /**
+   * This method build all structures and takes into account dependencies
+   * between them. Some structures need to be built before other ones.
+   * @param {array} structures AST structures
+   */
+  buildAllStructures(ast, structures, builtElements) {
+    const builtStructures = {};
+    const structuresToProcess = _.clone(structures);
+    while (structuresToProcess.length > 0) {
+      const builtStructuresDuringThisTurn = [];
+
+      // We have to check if any field depends on a structure not built yet
+      _.each(structuresToProcess, structureToProcess => {
+        let readyToBuild = true;
+        _.each(structureToProcess.fields, field => {
+          const typeName = field.type.type;
+          if (_.has(builtInTypes, typeName)) {
+            // Built-in type
+            return;
+          }
+          if (_.has(builtElements.bitmasks, typeName)) {
+            // Bitmask
+            return;
+          }
+          if (_.has(builtElements.enumerations, typeName)) {
+            // Enumeration
+            return;
+          }
+          if (_.has(builtStructures, typeName)) {
+            // Structure already built
+            return;
+          }
+          readyToBuild = false;
+          return false;
+        });
+
+        if (readyToBuild) {
+          builtStructuresDuringThisTurn.push(structureToProcess);
+          builtStructures[structureToProcess.name] = this.buildStructure(
+            ast,
+            {
+              structures: builtStructures,
+              enumerations: builtElements.enumerations,
+              bitmasks: builtElements.bitmasks,
+            },
+            structureToProcess
+          );
+        }
+      });
+
+      _.remove(structuresToProcess, s => _.includes(builtStructuresDuringThisTurn, s));
+
+      if (builtStructuresDuringThisTurn.length === 0) {
+        // We have just met a circular dependency, since we have just performed
+        // a rotation without changes.
+        throw new Error("Circular dependencies detected between structures");
+      }
+    }
+    return builtStructures;
   }
 
   buildStructure(ast, builtElements, structure) {
     const endiannessHeader = ast.headers.find(h => h.name === "endianness");
     const structureObject = new Structure(
       structure.name,
-      _.map(structure.fields, f => this.buildField(ast, builtElements, f))
+      _.map(structure.fields, f => this.buildField(builtElements, f))
     );
     if (!_.isUndefined(endiannessHeader)) {
       structureObject.setEndianness(endiannessHeader.value);
@@ -41,40 +102,29 @@ class DefinitionBuilder {
     return structureObject;
   }
 
-  buildEnumeration(ast, enumeration) {
+  buildEnumeration(enumeration) {
     const entries = _.map(enumeration.entries, this.buildEnumEntry.bind(this));
     return new Enumeration(enumeration.name, entries);
   }
 
-  buildBitmask(ast, bitmask) {
+  buildBitmask(bitmask) {
     const entries = _.map(bitmask.entries, this.buildBitmaskEntry.bind(this));
     return new Bitmask(bitmask.name, entries);
   }
 
-  buildField(ast, builtElements, field) {
+  buildField(builtElements, field) {
     const typeName = field.type.type;
     let type;
     if (_.has(builtInTypes, typeName)) {
-      // Built-in type
       type = builtInTypes[typeName](field.type);
+    } else if (_.has(builtElements.structures, typeName)) {
+      type = new StructureType(_.get(builtElements.structures, typeName));
+    } else if (_.has(builtElements.enumerations, typeName)) {
+      type = new EnumerationType(_.get(builtElements.enumerations, typeName));
+    } else if (_.has(builtElements.bitmasks, typeName)) {
+      type = new BitmaskType(_.get(builtElements.bitmasks, typeName));
     } else {
-      // TODO: Cache the "buildStructure" calls, otherwise each structure will be built several times
-      const foundStructure = _.find(ast.structures, s => s.name === typeName);
-      if (!_.isUndefined(foundStructure)) {
-        type = new StructureType(this.buildStructure(ast, builtElements, foundStructure));
-      } else {
-        const foundEnumeration = _.find(builtElements.enumerations, e => e.name === typeName);
-        if (!_.isUndefined(foundEnumeration)) {
-          type = new EnumerationType(foundEnumeration);
-        } else {
-          const foundBitmask = _.find(builtElements.bitmasks, b => b.name === typeName);
-          if (!_.isUndefined(foundBitmask)) {
-            type = new BitmaskType(foundBitmask);
-          } else {
-            throw new Error(`Unable to find referenced type ${typeName}`);
-          }
-        }
-      }
+      throw new Error(`Unable to find referenced type ${typeName}`);
     }
     if (_.has(field, "arrayDefinition")) {
       const definitionCode = `(function(variableScope) { return ${this.converter.convert(
