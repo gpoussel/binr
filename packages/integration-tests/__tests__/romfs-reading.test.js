6a515528392a05dc8c5fe5f1dc27b935ca394efc
Guillaume Poussel <gpoussel@gmail.com>
2019-02-05 23:13:33 +0100

    Adding ROMFS file format unit test.
    This file format is interesting because it contains C-style strings.

---
+++ packages/integration-tests/__tests__/romfs-reading.test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const fs = require("fs");
+const { BinaryReader } = require("@binr/binary-reader");
+const { DefinitionReader } = require("@binr/definition-reader");
+
+const pathToBinaryFixtures = `${__dirname}/../__fixtures__/binaries`;
+const pathToDefinitionFixtures = `${__dirname}/../__fixtures__/definitions`;
+
+describe("ROMFS reading", () => {
+  const definitionReader = new DefinitionReader();
+  const romfsDefinitionFile = fs.readFileSync(`${pathToDefinitionFixtures}/romfs.binr`);
+  const romfsDefinition = definitionReader.readInput(romfsDefinitionFile.toString());
+
+  const sampleRomfsFile = `${pathToBinaryFixtures}/sample.romfs`;
+  const sampleRomfsBuffer = fs.readFileSync(sampleRomfsFile);
+
+  test("reads ROMFS file with ROMFS definition", () => {
+    const binaryReader = new BinaryReader();
+    const value = binaryReader.read(sampleRomfsBuffer, romfsDefinition, "RomfsFile");
+    expect(value).toBeDefined();
+    const { header, entry } = value;
+    expect(header).toBeDefined();
+    expect(header.magicBytes).toEqual("-rom1fs-".split(""));
+    expect(header.size).toBe(272);
+    expect(header.checksum).toBe(1391339101);
+    expect(header.volumeName).toEqual("DEFALIGNED");
+
+    expect(entry).toBeDefined();
+
+    const { header: entryHeader } = entry;
+    expect(entryHeader).toBeDefined();
+    // expect(entryHeader.nextEntryPosition).toBe(1);
+    // expect(entryHeader.executable).toBe(1);
+    // expect(entryHeader.type).toEqual("BLOCK_DEV");
+  });
+});
