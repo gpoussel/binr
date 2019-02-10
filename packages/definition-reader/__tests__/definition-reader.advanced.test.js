467ad0e45edcc66d1f3e75b827687d6d193521e4
Guillaume Poussel <gpoussel@gmail.com>
2019-02-10 11:23:46 +0100

    Remove Java CLASS test from advanced DefinitionReader tests

--- packages/definition-reader/__tests__/definition-reader.advanced.test.js
+++ packages/definition-reader/__tests__/definition-reader.advanced.test.js
@@ -10,7 +10,7 @@ describe("DefinitionReader, advanced", () => {
   // Note: these definitions are not complete at all. They are often inspired by
   // a real-world format but they are never fully implemented.
   test(`parses example format definition`, () => {
-    _.each(["class.binr", "bmp.binr", "mft.binr"], filename => {
+    _.each(["bmp.binr", "mft.binr"], filename => {
       const structure = fs.readFileSync(`${pathToFixtures}/${filename}`, "utf-8");
       const reader = new DefinitionReader();
       const definition = reader.readInput(structure);
