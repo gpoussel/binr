883335edb49b4da5f6d6fc2c4f068c007851848d
Guillaume Poussel <gpoussel@gmail.com>
2019-02-10 16:14:32 +0100

    Improve Java CLASS binary structure and unit test.

--- packages/integration-tests/__tests__/javaclass-reading.test.js
+++ packages/integration-tests/__tests__/javaclass-reading.test.js
@@ -13,10 +13,10 @@ describe("JAVA Class reading", () => {
   const javaClassDefinitionFile = fs.readFileSync(`${pathToDefinitionFixtures}/java_class.binr`);
   const javaClassDefinition = definitionReader.readInput(javaClassDefinitionFile.toString());
 
-  const sampleJavaClassFile = `${pathToBinaryFixtures}/CucumberModules.class`;
-  const sampleJavaClassBuffer = fs.readFileSync(sampleJavaClassFile);
+  test("reads CumcumberModules.class file", () => {
+    const sampleJavaClassFile = `${pathToBinaryFixtures}/CucumberModules.class`;
+    const sampleJavaClassBuffer = fs.readFileSync(sampleJavaClassFile);
 
-  test("reads class file", () => {
     const binaryReader = new BinaryReader();
     const value = binaryReader.read(sampleJavaClassBuffer, javaClassDefinition, "JVMClass");
     expect(value).toBeDefined();
@@ -56,14 +56,111 @@ describe("JAVA Class reading", () => {
       "()Lcucumber/runtime/java/guice/ScenarioScope;",
       "(Lcucumber/runtime/java/guice/ScenarioScope;)V",
     ]);
-    expect(value.accessFlags).toEqual(["ACC_PUBLIC"]);
+    expect(value.accessFlags).toEqual(["ACC_PUBLIC", "ACC_SUPER"]);
     expect(value.thisClass).toEqual(7);
     expect(value.superClass).toEqual(8);
     expect(value.interfacesCount).toEqual(0);
     expect(value.interfaces).toHaveLength(0);
     expect(value.fieldsCount).toEqual(1);
-    expect(value.fields).toEqual([25]);
-    expect(value.attributesCount).toEqual(9);
-    expect(value.attributes).toEqual([10, 2, 11, 0, 0, 12, 0, 6, 1]);
+    expect(value.fields[0]).toEqual({
+      accessFlags: ["ACC_PUBLIC", "ACC_STATIC", "ACC_FINAL"],
+      attributes: [
+        {
+          attributeLength: 0,
+          attributeNameIndex: 11,
+          info: [],
+        },
+        {
+          attributeLength: 6,
+          attributeNameIndex: 12,
+          info: [0, 1, 0, 13, 0, 0],
+        },
+      ],
+      attributesCount: 2,
+      descriptorIndex: 10,
+      nameIndex: 9,
+    });
+    expect(value.methodsCount).toEqual(4);
+    expect(value.methods[0]).toEqual({
+      accessFlags: ["ACC_PUBLIC"],
+      attributes: [
+        {
+          attributeLength: 47,
+          attributeNameIndex: 16,
+          info: [
+            0,
+            1,
+            0,
+            1,
+            0,
+            0,
+            0,
+            5,
+            42,
+            183,
+            0,
+            1,
+            177,
+            0,
+            0,
+            0,
+            2,
+            0,
+            17,
+            0,
+            0,
+            0,
+            6,
+            0,
+            1,
+            0,
+            0,
+            0,
+            12,
+            0,
+            18,
+            0,
+            0,
+            0,
+            12,
+            0,
+            1,
+            0,
+            0,
+            0,
+            5,
+            0,
+            19,
+            0,
+            20,
+            0,
+            0,
+          ],
+        },
+      ],
+      attributesCount: 1,
+      descriptorIndex: 15,
+      nameIndex: 14,
+    });
+    expect(value.attributesCount).toEqual(1);
+    expect(value.attributes).toEqual([
+      {
+        attributeLength: 2,
+        attributeNameIndex: 27,
+        info: [0, 28],
+      },
+    ]);
+  });
+
+  test("reads SmartLeaves.class file", () => {
+    const sampleJavaClassFile = `${pathToBinaryFixtures}/SmartLeaves.class`;
+    const sampleJavaClassBuffer = fs.readFileSync(sampleJavaClassFile);
+
+    const binaryReader = new BinaryReader();
+    const value = binaryReader.read(sampleJavaClassBuffer, javaClassDefinition, "JVMClass");
+    expect(value).toBeDefined();
+    expect(value.magic).toBe(0xcafebabe);
+    expect(value.majorVersion).toBe(52);
+    expect(value.minorVersion).toBe(0);
   });
 });
