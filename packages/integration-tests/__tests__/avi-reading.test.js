f5e289e86a5bf2b791c6837d065f894cc517f3ef
Guillaume Poussel <gpoussel@gmail.com>
2019-02-11 22:25:54 +0100

    Begin implementation of AVI format.
    This format will highlight:
    - end-of-file loops
    - reading char arrays as strings

---
+++ packages/integration-tests/__tests__/avi-reading.test.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require("fs");
+const { BinaryReader } = require("@binr/binary-reader");
+const { DefinitionReader } = require("@binr/definition-reader");
+
+const pathToBinaryFixtures = `${__dirname}/../__fixtures__/binaries`;
+const pathToDefinitionFixtures = `${__dirname}/../__fixtures__/definitions`;
+
+describe("AVI reading", () => {
+  const definitionReader = new DefinitionReader();
+
+  const aviDefinitionFile = fs.readFileSync(`${pathToDefinitionFixtures}/avi.binr`);
+  const aviDefinition = definitionReader.readInput(aviDefinitionFile.toString());
+
+  const aviFile = `${pathToBinaryFixtures}/MVI_3572.AVI`;
+  const aviBuffer = fs.readFileSync(aviFile);
+
+  test("reads AVI file", () => {
+    const binaryReader = new BinaryReader();
+    const aviValue = binaryReader.read(aviBuffer, aviDefinition, "AviFile");
+    expect(aviValue).toBeDefined();
+
+    const { root } = aviValue;
+    expect(root.id).toEqual("RIFF".split(""));
+    expect(root.datalen).toBe(904606);
+    expect(root.form).toEqual("AVI ".split(""));
+    console.log(aviValue);
+  });
+});
