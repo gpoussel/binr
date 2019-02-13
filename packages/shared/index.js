218eecab9fd45f9c23d0e34d9046a8ebff2d2ee3
Guillaume Poussel <gpoussel@gmail.com>
2019-02-13 23:02:54 +0100

    Sort objects by lexicographical order.

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -3,9 +3,11 @@
 const BufferWrapper = require("./lib/buffer-wrapper");
 const VariableScope = require("./lib/variable-scope");
 const FunctionScope = require("./lib/function-scope");
+const Environment = require("./lib/environment");
 
 module.exports = {
   BufferWrapper,
   VariableScope,
   FunctionScope,
+  Environment,
 };
