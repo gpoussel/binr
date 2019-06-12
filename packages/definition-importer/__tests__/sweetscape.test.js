a1f051e9eccc617c925c0024ea2d96ac17a020bd
Guillaume Poussel <gpoussel@gmail.com>
2019-06-12 21:56:42 +0200

    Add another test fixture to improve coverage

--- packages/definition-importer/__tests__/sweetscape.test.js
+++ packages/definition-importer/__tests__/sweetscape.test.js
@@ -8,7 +8,7 @@ const structureFolder = `${__dirname}/../__fixtures__/`;
 
 describe("Sweetscape Importer", () => {
   const importer = new SweetscapeImporter();
-  _.each(["swf-sample"], name => {
+  _.each(["swf-sample", "vbmeta-sample"], name => {
     test(`reads ${name} definition`, () => {
       const definitionBuffer = fs.readFileSync(`${structureFolder}/${name}.bt`, "UTF-8");
       const definition = importer.readInput(definitionBuffer);
