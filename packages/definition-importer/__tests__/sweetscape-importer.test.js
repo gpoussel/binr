953297e3b1022bd8e63b414e9e2eb9301c301d09
Guillaume Poussel <guillaume.poussel@sogeti.com>
2019-06-15 12:12:16 +0200

    Start implementation of Importer test

--- packages/definition-importer/__tests__/sweetscape-importer.test.js
+++ packages/definition-importer/__tests__/sweetscape-importer.test.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const _ = require("lodash");
 const SweetscapeImporter = require("../lib/sweetscape/sweetscape-importer");
 const { iterateStructures } = require("./utils/010-structures");
 
@@ -11,6 +12,133 @@ describe("Sweetscape Importer", () => {
       (name, input) => {
         const definition = importer.readInput(input);
         expect(definition).toBeDefined();
+        expect(definition.type).toEqual("definition");
+        const { content } = definition;
+
+        expect(content).toHaveLength(93);
+
+        const firstNode = content[0];
+        expect(firstNode).toEqual({
+          type: "typeAlias",
+          name: {
+            name: "QWORD",
+            array: false,
+          },
+          alias: "ULONGLONG",
+        });
+
+        const localVarDeclaration = content[75];
+        expect(localVarDeclaration).toEqual({
+          type: "variableDeclaration",
+          variableType: {
+            name: "ULONG",
+            array: false,
+          },
+          declarations: [], // TODO That's wrong
+          // TODO Missing "local"
+        });
+
+        const ifStatement = content[76];
+        expect(ifStatement).toEqual({
+          type: "ifStatement",
+          condition: {
+            type: "primaryExpression",
+            expression: {
+              name: "NtHeader",
+              type: "identifier",
+              // TODO That's wrong
+            },
+          },
+          trueStatement: {
+            type: "statementList",
+            statements: [
+              {
+                type: "variableDeclaration",
+                variableType: {
+                  array: false, // TODO That's wrong
+                  name: "UCHAR",
+                },
+                declarations: [], // TODO That's wrong
+                // TODO Missing comments
+              },
+            ],
+          },
+        });
+
+        const printfSizeofFunctionCall = content[77];
+        expect(printfSizeofFunctionCall).toEqual({
+          type: "primaryExpression",
+          expression: {
+            type: "functionCall",
+            name: "Printf",
+            arguments: [
+              {
+                type: "primaryExpression",
+                expression: {
+                  type: "stringLiteral",
+                  string: "Space between headers and first sections is %d bytes\\n",
+                },
+              },
+              {
+                type: "sizeofExpression",
+                expression: {
+                  type: "primaryExpression",
+                  expression: {
+                    name: "Space2",
+                    type: "identifier",
+                  },
+                },
+              },
+            ],
+          },
+        });
+
+        const fseekFunctionCall = content[78];
+        expect(fseekFunctionCall).toEqual({
+          type: "primaryExpression",
+          expression: {
+            type: "functionCall",
+            name: "FSeek",
+            arguments: [
+              {
+                type: "primaryExpression",
+                expression: {
+                  type: "identifier",
+                  name: "NtHeader",
+                  // TODO: That's wrong, but that's the current output
+                },
+              },
+            ],
+          },
+        });
+
+        const simpleFunctionCall = content[91];
+        expect(simpleFunctionCall).toEqual({
+          type: "primaryExpression",
+          expression: {
+            type: "functionCall",
+            name: "ParseDelayImport",
+            arguments: [],
+          },
+        });
+
+        const lastPrintfNode = content[92];
+        expect(lastPrintfNode).toEqual({
+          type: "primaryExpression",
+          expression: {
+            type: "functionCall",
+            name: "Printf",
+            arguments: [
+              {
+                type: "primaryExpression",
+                expression: {
+                  type: "stringLiteral",
+                  string: "EXE.bt finished\\n",
+                },
+              },
+            ],
+          },
+        });
       },
       done,
       name => name === "EXE.bt"
