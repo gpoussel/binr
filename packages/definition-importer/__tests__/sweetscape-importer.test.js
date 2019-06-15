bcb2a53b278e221d96f0a049aa25ce4ba1fb4e13
Guillaume Poussel <guillaume.poussel@sogeti.com>
2019-06-15 18:57:33 +0200

    Reorganize unit tests and start using snapshots

--- packages/definition-importer/__tests__/sweetscape-importer.test.js
+++ packages/definition-importer/__tests__/sweetscape-importer.test.js
@@ -1,10 +1,38 @@
 "use strict";
 
 const SweetscapeImporter = require("../lib/sweetscape/sweetscape-importer");
-const { iterateStructures } = require("./utils/010-structures");
+const { iterateStructures, getSingleStructure } = require("./utils/010-structures");
 
 describe("Sweetscape Importer", () => {
   const importer = new SweetscapeImporter();
+
+  test("creates misc definitions", () => {
+    // The file itself is not important and does not represent any structures
+    // It is only there to reach a 100% coverage on the structure parser
+    // This is definitely useful for regression test during refactoring
+    const definition = importer.readInput(getSingleStructure("misc-sample"));
+    expect(definition).toBeDefined();
+  });
+
+  test("creates all 010 sample definitions", done => {
+    // We are keeping the state right there, so we can stop the iteration
+    // as soon as we got a failure
+    let hasFailed = false;
+    iterateStructures(
+      (name, input) => {
+        try {
+          const definition = importer.readInput(input);
+          expect(definition).toBeDefined();
+        } catch (e) {
+          e.message = `[${name}] ${e.message}`;
+          done.fail(e);
+          hasFailed = true;
+        }
+      },
+      done,
+      () => !hasFailed
+    );
+  }, 45000);
 
   test("creates EXE definition", done => {
     iterateStructures(
--- packages/definition-importer/__tests__/sweetscape-parser.test.js
+++ packages/definition-importer/__tests__/sweetscape-parser.test.js
@@ -1,35 +1,122 @@
 "use strict";
 
-const SweetscapeImporter = require("../lib/sweetscape/sweetscape-importer");
-const { getSingleStructure, iterateStructures } = require("./utils/010-structures");
+const _ = require("lodash");
+const SweetscapeLexer = require("../lib/sweetscape/sweetscape-lexer");
+const SweetscapeParser = require("../lib/sweetscape/sweetscape-parser");
 
-describe("Sweetscape Parser", () => {
-  const importer = new SweetscapeImporter();
-  test("reads misc definitions", () => {
-    // The file itself is not important and does not represent any structures
-    // It is only there to reach a 100% coverage on the structure parser
-    // This is definitely useful for regression test during refactoring
-    const definition = importer.readInput(getSingleStructure("misc-sample"));
-    expect(definition).toBeDefined();
+/**
+ * This function executes the parser over input and returns the parser itself (holding the parsing result)
+ * @param {string} input input text representing the input definition
+ * @return {object} parsing result
+ */
+function parse(input) {
+  const lexer = new SweetscapeLexer();
+  const parser = new SweetscapeParser();
+
+  const lexingResult = lexer.tokenize(input);
+  if (!_.isEmpty(lexingResult.errors)) {
+    throw new Error(`Got an error while lexing input: ${_.first(lexingResult.errors).message}`);
+  }
+  parser.input = lexingResult.tokens;
+  return parser;
+}
+
+/**
+ * Tidy up the parsing result to remove chevrotain-related stuff.
+ * @param {object} object input object (parsing result)
+ * @return {object} cleaned-up objeects
+ */
+function cleanupResult(object) {
+  if (_.has(object, "tokenType")) {
+    // This is a token, and we don't want to serialize all its properties
+    return { TOKEN: object.tokenType.tokenName, content: object.image };
+  }
+  _.each(object, (value, key) => {
+    if (_.isArray(value)) {
+      if (_.size(value) === 1) {
+        // To improve readability, remove the array and put a single object
+        object[key] = cleanupResult(_.first(value));
+      } else {
+        object[key] = _.map(value, cleanupResult);
+      }
+    } else if (_.isObject(value)) {
+      const result = cleanupResult(value);
+      object[key] = result;
+    }
   });
 
-  test("reads all 010 sample structures", done => {
-    // We are keeping the state right there, so we can stop the iteration
-    // as soon as we got a failure
-    let hasFailed = false;
-    iterateStructures(
-      (name, input) => {
-        try {
-          const definition = importer.readInput(input);
-          expect(definition).toBeDefined();
-        } catch (e) {
-          e.message = `[${name}] ${e.message}`;
-          done.fail(e);
-          hasFailed = true;
-        }
-      },
-      done,
-      () => !hasFailed
-    );
-  }, 45000);
+  if (_.has(object, "children")) {
+    return object.children;
+  }
+  return object;
+}
+
+describe("Sweetscape Parser", () => {
+  _.each(
+    [
+      { name: "variable definition", input: "int a;" },
+      { name: "several variable definitions", input: "int a, b;" },
+      { name: "variable definition with initializer", input: "int a = 1;" },
+      { name: "annotated variable definition", input: "int a <format=hex>;" },
+      { name: "local variable definition", input: "local int a;" },
+      { name: "const variable definition", input: "const int a;" },
+      { name: "variable with constant bitfield", input: "int a : 3;" },
+      { name: "variable with identifier as bitfield", input: "int a : NBits;" },
+      { name: "variable with expression as bitfield", input: "int a : NBits / 2;" },
+      { name: "anonymous variable with bitfield", input: "int : 3;" },
+      { name: "annotated anonymous variable with bitfield", input: "int : 3 <format=hex>;" },
+      { name: "array variable definition", input: "int[] a;" },
+      { name: "array variable definition with initializer", input: "int[] a = { 1 , 2 };" },
+    ],
+    testDefinition => {
+      test(`parses statement (${testDefinition.name})`, () => {
+        const parsingResult = parse(testDefinition.input);
+        const statement = parsingResult.statement();
+        expect(cleanupResult(statement)).toMatchSnapshot();
+      });
+    }
+  );
+  _.each(
+    [
+      { name: "hex number 1", input: "0x20" },
+      { name: "hex number 2", input: "20h" },
+      { name: "decimal number", input: "10" },
+      { name: "binary number", input: "0b10" },
+      { name: "octal number", input: "045" },
+      { name: "string", input: "'a'" },
+      { name: "prefixOperation", input: "!a" },
+      { name: "infixOperation", input: "a+b" },
+      { name: "postfixOperation", input: "a++" },
+      { name: "assignment", input: "a = b" },
+      { name: "ternary", input: "a ? 1 : 0" },
+      { name: "selector", input: "a.b[c]" },
+      { name: "sizeof type", input: "sizeof(int)" },
+      { name: "sizeof expression", input: "sizeof(a[0])" },
+      { name: "function call", input: "a(1, 2)" },
+      { name: "cast", input: "(int) b" },
+      { name: "parenthesized expression", input: "(a + b)" },
+    ],
+    testDefinition => {
+      test(`parses expression (${testDefinition.name})`, () => {
+        const parsingResult = parse(testDefinition.input);
+        const statement = parsingResult.expression();
+        expect(cleanupResult(statement)).toMatchSnapshot();
+      });
+    }
+  );
+  _.each(
+    [
+      { name: "operator priority", input: "a * b + c" },
+      { name: "operator priority with parenthesis", input: "(a * b) + c" },
+      { name: "operator priority with parenthesis and cast", input: "(int) (a * b) + c" },
+      { name: "operator priority with cast", input: "(int) fn(a) + 1" },
+    ],
+    testDefinition => {
+      test(`parses complex expression (${testDefinition.name})`, () => {
+        const parsingResult = parse(testDefinition.input);
+        const statement = parsingResult.expression();
+        expect(cleanupResult(statement)).toMatchSnapshot();
+      });
+    }
+  );
 });
