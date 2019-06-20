30bdad7fdd0d1378274e6d2a2eff9d1d9aade38d
Guillaume Poussel <guillaume.poussel@sogeti.com>
2019-06-20 20:20:15 +0200

    Migrate @binr/binary-reader tests

--- packages/binary-reader/__tests__/binary-reader.test.ts
+++ packages/binary-reader/__tests__/binary-reader.test.ts
@@ -1,10 +1,10 @@
 "use strict";
 
-const BinaryReader = require("../lib/binary-reader");
-const { Definition, Structure } = require("@binr/model");
+import { Definition, Structure } from "@binr/model";
+import { BinaryReader } from "../lib/binary-reader";
 
 const createEmptyBuffer = () => Buffer.from([]);
-const createEmptyStructure = name => {
+const createEmptyStructure = (name) => {
   const structure = new Structure(name, []);
   structure.setEndianness("big");
   return structure;
@@ -14,14 +14,14 @@ describe("BinaryReader", () => {
   test("throws an error when no structure defined", () => {
     const binaryReader = new BinaryReader();
     const buffer = createEmptyBuffer();
-    const definition = new Definition([]);
+    const definition = new Definition([], [], []);
     expect(() => binaryReader.read(buffer, definition)).toThrow();
   });
 
   test("uses the only existing structure without giving its name", () => {
     const binaryReader = new BinaryReader();
     const buffer = createEmptyBuffer();
-    const definition = new Definition([createEmptyStructure("MyStructure")]);
+    const definition = new Definition([createEmptyStructure("MyStructure")], [], []);
     const result = binaryReader.read(buffer, definition);
     expect(result).toEqual({});
   });
