a8f4de3026d907be1d424c2c8e64ab29711fb660
Guillaume Poussel <gpoussel@gmail.com>
2019-06-20 16:52:02 +0200

    Use Jest snapshots for all integration tests

--- packages/integration-tests/__tests__/ico-reading.test.js
+++ packages/integration-tests/__tests__/ico-reading.test.js
@@ -22,37 +22,6 @@ describe("ICO reading", () => {
     const binaryReader = new BinaryReader();
     const icoValue = binaryReader.read(icoBuffer, icoDefinition, "IcoFile");
     expect(icoValue).toBeDefined();
-
-    const { iconDir } = icoValue;
-    expect(iconDir).toBeDefined();
-    expect(iconDir.reserved).toBe(0);
-    expect(iconDir.type).toBe(1);
-    expect(iconDir.count).toBe(2);
-
-    const expectedSizes = [16, 32];
-    const { entries } = iconDir;
-    expect(entries.map(e => e.width)).toEqual(expectedSizes);
-    expect(entries.map(e => e.height)).toEqual(expectedSizes);
-    expect(entries.map(e => e.colorCount)).toEqual(_.times(2, () => 0));
-    expect(entries.map(e => e.reserved)).toEqual(_.times(2, () => 0));
-    expect(entries.map(e => e.planes)).toEqual(_.times(2, () => 1));
-    expect(entries.map(e => e.bitCount)).toEqual(_.times(2, () => 32));
-
-    const { images } = icoValue;
-    expect(images).toHaveLength(1);
-    const [firstImage] = images;
-    expect(firstImage.header).toEqual({
-      size: 40,
-      width: 16,
-      height: 32,
-      planes: 1,
-      bitCount: 32,
-      compression: 0,
-      sizeImage: 0,
-      xPelsPerMeter: 0,
-      yPelsPerMeter: 0,
-      clrUsed: 0,
-      clrImportant: 0,
-    });
+    expect(icoValue).toMatchSnapshot();
   });
 });
