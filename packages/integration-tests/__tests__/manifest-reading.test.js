ca86a840a86deae67a660d938500df46d45c7ed3
Guillaume Poussel <gpoussel@gmail.com>
2019-06-20 17:05:59 +0200

    Use Jest snapshot

--- packages/integration-tests/__tests__/manifest-reading.test.js
+++ packages/integration-tests/__tests__/manifest-reading.test.js
@@ -20,39 +20,6 @@ describe("Android Manifest reading", () => {
     const binaryReader = new BinaryReader();
     const manifest = binaryReader.read(manifestBuffer, manifestDefinition, "ManifestFile");
     expect(manifest).toBeDefined();
-
-    const { header } = manifest;
-    expect(header.magicNumber).toBe(524291);
-    expect(header.fileSize).toBe(1324);
-
-    const { stringChunk } = manifest;
-    expect(stringChunk.signature).toBe(1835009);
-    expect(stringChunk.size).toBe(724);
-    expect(stringChunk.stringCount).toBe(19);
-    expect(stringChunk.styleCount).toBe(0);
-    expect(stringChunk.stringPoolOffset).toBe(104);
-    expect(stringChunk.stylePoolOffset).toBe(0);
-    expect(stringChunk.stringOffsets).toEqual([
-      0,
-      26,
-      82,
-      96,
-      108,
-      126,
-      214,
-      218,
-      236,
-      256,
-      322,
-      354,
-      380,
-      400,
-      428,
-      458,
-      474,
-      530,
-      550,
-    ]);
-    expect(stringChunk.styleOffsets).toHaveLength(0);
+    expect(manifest).toMatchSnapshot();
   });
 });
