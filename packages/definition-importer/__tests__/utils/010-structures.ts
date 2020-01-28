dcec1952671f35e6a27c0c7cf154aa7b1ed2e609
Guillaume Poussel <guillaume.poussel@sogeti.com>
2020-01-28 16:17:10 +0100

    Split 010 importer tests (so they can be executed in parallel)

--- packages/definition-importer/__tests__/utils/010-structures.ts
+++ packages/definition-importer/__tests__/utils/010-structures.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 // @ts-ignore
 import * as gunzip from "gunzip-maybe";
-import { isUndefined, join } from "lodash";
+import { join } from "lodash";
 import * as tar from "tar-stream";
 
 const structureFolder = `${__dirname}/../../__fixtures__/`;
@@ -10,57 +10,27 @@ export function getSingleStructure(name: string) {
   return fs.readFileSync(`${structureFolder}/${name}.bt`, "UTF-8");
 }
 
-export function iterateArchive(
-  filename: string,
-  iteratee: (name: string, buffer: string) => void,
-  done: () => void,
-  filter: (name: string) => boolean,
-) {
+export async function getArchiveEntries(filename: string) {
   const readStream = fs.createReadStream(`${structureFolder}/${filename}`);
-  const extract = tar.extract();
-
-  extract.on("entry", (header, stream, next) => {
-    const chunks: string[] = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.once("end", () => {
-      const buffer = join(chunks, "");
-      if (isUndefined(filter) || filter(header.name)) {
-        iteratee(header.name, buffer);
-      }
-      next();
+  return new Promise<any[]>((resolve) => {
+    const extract = tar.extract();
+    const entries: any[] = [];
+    extract.on("entry", (header, stream, next) => {
+      const chunks: string[] = [];
+      stream.on("data", (chunk) => chunks.push(chunk));
+      stream.once("end", () => {
+        const buffer = join(chunks, "");
+        entries.push({
+          name: header.name,
+          content: buffer,
+        });
+        next();
+      });
+      stream.resume();
     });
-    stream.resume();
+    extract.on("finish", () => {
+      resolve(entries);
+    });
+    readStream.pipe(gunzip()).pipe(extract);
   });
-  extract.on("finish", () => {
-    done();
-  });
-  readStream.pipe(gunzip()).pipe(extract);
-}
-
-export function iterateStructures(
-  iteratee: (name: string, buffer: string) => void,
-  done: () => void,
-  filter: (name: string) => boolean,
-) {
-  return iterateArchive("010-structures.tar.gz", iteratee, done, filter);
-}
-
-export function getSingleStructureInArchive(
-  name: string,
-  iteratee: (input: string) => void,
-  done: () => void,
-) {
-  return iterateStructures(
-    (_name: string, buffer: string) => iteratee(buffer),
-    done,
-    (structureName) => structureName === name,
-  );
-}
-
-export function iterateScripts(
-  iteratee: (name: string, buffer: string) => void,
-  done: () => void,
-  filter: (name: string) => boolean,
-) {
-  return iterateArchive("010-scripts.tar.gz", iteratee, done, filter);
 }
