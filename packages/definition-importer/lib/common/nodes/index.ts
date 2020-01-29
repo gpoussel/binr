272f80780e3b03fac490437d1508c7fccb7a4f57
Guillaume Poussel <guillaume.poussel@sogeti.com>
2020-01-29 11:33:43 +0100

    Fix index.ts files

--- packages/definition-importer/lib/common/nodes/index.ts
+++ packages/definition-importer/lib/common/nodes/index.ts
@@ -1,111 +1,54 @@
-import { Annotation } from "./annotation";
-import { ArrayIndexExpression } from "./array-index-expression";
-import { ArrayInitializationExpression } from "./array-initialization-expression";
-import { ArraySelector } from "./array-selector";
-import { BinaryExpression } from "./binary-expression";
-import { BlockStatement } from "./block-statement";
-import { BooleanValue } from "./boolean-value";
-import { BreakStatement } from "./break-statement";
-import { CaseSwitchElement } from "./case-switch-element";
-import { CastExpression } from "./cast-expression";
-import { CommaExpression } from "./comma-expression";
-import { DefaultSwitchLabel } from "./default-switch-label";
-import { Definition } from "./definition";
-import { DoWhileStatement } from "./do-while-statement";
-import { EmptyArraySelector } from "./empty-array-selector";
-import { EmptyStatement } from "./empty-statement";
-import { EnumDeclarationElement } from "./enum-declaration-element";
-import { EnumDeclarationStatement } from "./enum-declaration-statement";
-import { Expression } from "./expression";
-import { ExpressionArraySelector } from "./expression-array-selector";
-import { ExpressionStatement } from "./expression-statement";
-import { ForStatement } from "./for-statement";
-import { FunctionCallExpression } from "./function-call-expression";
-import { FunctionDeclarationStatement } from "./function-declaration-statement";
-import { IdentifierValue } from "./identifier-value";
-import { IfStatement } from "./if-statement";
-import { NamedType } from "./named-type";
-import { Node } from "./node";
-import { NumberValue } from "./number-value";
-import { Operator } from "./operator";
-import { ParameterDeclaration } from "./parameter-declaration";
-import { PostfixExpression } from "./postfix-expression";
-import { PrefixExpression } from "./prefix-expression";
-import { PropertyAccessExpression } from "./property-access-expression";
-import { ReturnStatement } from "./return-statement";
-import { SizeofExpression } from "./sizeof-expression";
-import { Statement } from "./statement";
-import { StringValue } from "./string-value";
-import { StructDeclarationStatement } from "./struct-declaration-statement";
-import { SwitchLabel } from "./switch-label";
-import { SwitchStatement } from "./switch-statement";
-import { TernaryExpression } from "./ternary-expression";
-import { Type } from "./type";
-import { TypeModifier } from "./type-modifier";
-import { TypedefStatement } from "./typedef-statement";
-import { UnaryExpression } from "./unary-expression";
-import { UnionDeclarationStatement } from "./union-declaration-statement";
-import { Value } from "./value";
-import { ValueSwitchLabel } from "./value-switch-label";
-import { VariableDeclaration } from "./variable-declaration";
-import { VariableDeclarationStatement } from "./variable-declaration-statement";
-import { VariableModifier } from "./variable-modifier";
-import { VoidType } from "./void-type";
-import { WhileStatement } from "./while-statement";
-
-export {
-  Annotation,
-  BinaryExpression,
-  BlockStatement,
-  BooleanValue,
-  CastExpression,
-  BreakStatement,
-  CommaExpression,
-  PostfixExpression,
-  PrefixExpression,
-  Definition,
-  EmptyStatement,
-  FunctionCallExpression,
-  ArrayIndexExpression,
-  Expression,
-  ExpressionStatement,
-  IdentifierValue,
-  NamedType,
-  Node,
-  NumberValue,
-  Operator,
-  SizeofExpression,
-  Statement,
-  StringValue,
-  TernaryExpression,
-  Type,
-  TypeModifier,
-  UnaryExpression,
-  Value,
-  VariableDeclarationStatement,
-  VariableModifier,
-  VoidType,
-  PropertyAccessExpression,
-  ArrayInitializationExpression,
-  ReturnStatement,
-  WhileStatement,
-  DoWhileStatement,
-  ForStatement,
-  IfStatement,
-  CaseSwitchElement,
-  DefaultSwitchLabel,
-  SwitchLabel,
-  SwitchStatement,
-  ValueSwitchLabel,
-  EnumDeclarationStatement,
-  VariableDeclaration,
-  ArraySelector,
-  EmptyArraySelector,
-  ExpressionArraySelector,
-  EnumDeclarationElement,
-  TypedefStatement,
-  StructDeclarationStatement,
-  UnionDeclarationStatement,
-  ParameterDeclaration,
-  FunctionDeclarationStatement,
-};
+export * from "./annotation";
+export * from "./array-index-expression";
+export * from "./array-initialization-expression";
+export * from "./array-selector";
+export * from "./binary-expression";
+export * from "./block-statement";
+export * from "./boolean-value";
+export * from "./break-statement";
+export * from "./case-switch-element";
+export * from "./cast-expression";
+export * from "./comma-expression";
+export * from "./default-switch-label";
+export * from "./definition";
+export * from "./do-while-statement";
+export * from "./empty-array-selector";
+export * from "./empty-statement";
+export * from "./enum-declaration-element";
+export * from "./enum-declaration-statement";
+export * from "./expression";
+export * from "./expression-array-selector";
+export * from "./expression-statement";
+export * from "./for-statement";
+export * from "./function-call-expression";
+export * from "./function-declaration-statement";
+export * from "./identifier-value";
+export * from "./if-statement";
+export * from "./named-type";
+export * from "./node";
+export * from "./number-value";
+export * from "./operator";
+export * from "./parameter-declaration";
+export * from "./postfix-expression";
+export * from "./prefix-expression";
+export * from "./property-access-expression";
+export * from "./return-statement";
+export * from "./sizeof-expression";
+export * from "./statement";
+export * from "./string-value";
+export * from "./struct-declaration-statement";
+export * from "./switch-label";
+export * from "./switch-statement";
+export * from "./ternary-expression";
+export * from "./type";
+export * from "./type-modifier";
+export * from "./typedef-statement";
+export * from "./unary-expression";
+export * from "./union-declaration-statement";
+export * from "./value";
+export * from "./value-switch-label";
+export * from "./variable-declaration";
+export * from "./variable-declaration-statement";
+export * from "./variable-modifier";
+export * from "./void-type";
+export * from "./while-statement";
