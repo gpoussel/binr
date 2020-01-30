209dcf437f0dce26359136815f438ad3f7f849f3
Guillaume Poussel <gpoussel@gmail.com>
2020-01-30 13:14:31 +0100

    Updates binr-reader-test

--- packages/definition-importer/__tests__/binr/binr-reader.test.ts
+++ packages/definition-importer/__tests__/binr/binr-reader.test.ts
@@ -15,57 +15,55 @@ describe("BinrDefinitionImporter", () => {
     expect(createAndCallParser("👍")).toThrow(/lexing/);
   });
 
-  test("throws an error when parsing invalid input", () => {
-    each(["#bar", "struct {}", "123", "beep", "##"], (value: string) => {
+  each(["#bar", "struct {}", "123", "beep", "##"], (value: string) => {
+    test(`throws an error when parsing invalid input (${value})`, () => {
       expect(createAndCallParser(value)).toThrow(/parsing/);
     });
   });
 
-  test("throws an error on invalid content", () => {
-    each(
-      [
-        "struct a {} struct a {}",
-        "struct a {int a; int a;}",
-        "",
-        '#set "b" #set "b" struct a {}',
-        "struct int {}",
-        "struct a { b b; }",
-        "struct c { int:0 a; }",
-        "struct c { int:255 a; }",
-        "struct c { string a; }",
-      ],
-      (value: string) => {
+  each(
+    [
+      "struct a {} struct a {}",
+      "struct a {int a; int a;}",
+      "",
+      '#set "b" #set "b" struct a {}',
+      "struct int {}",
+      "struct a { b b; }",
+      "struct c { int:0 a; }",
+      "struct c { int:255 a; }",
+      "struct c { string a; }",
+    ],
+    (value: string) => {
+      test(`throws an error on invalid content (${value})`, () => {
         expect(createAndCallParser(value)).toThrow(/validation/i);
-      },
-    );
-  });
+      });
+    },
+  );
 
-  test("accepts minimal input", () => {
-    each(['#set "foo" struct a {}', "#set 32 struct b {}", "struct a {}"], (value: string) => {
-      expect(createAndCallParser(value)()).toBeDefined();
+  each(['#set "foo" struct a {}', "#set 32 struct b {}", "struct a {}"], (value: string) => {
+    test(`accepts minimal input (${value})`, () => {
+      const result = createAndCallParser(value)();
+      expect(result).toBeDefined();
+      expect(result).toMatchSnapshot();
     });
   });
 
   test("accepts field annotations", () => {
-    each(["struct a { @ignore(false) int b; @ignore(true) int c; }"], (value: string) => {
-      expect(createAndCallParser(value)()).toBeDefined();
-    });
+    const result = createAndCallParser("struct a { @ignore(false) int b; @ignore(true) int c; }")();
+    expect(result).toBeDefined();
+    expect(result).toMatchSnapshot();
   });
 
   test("accepts bitmasks", () => {
-    each(["bitmask foo extends int:16 { A = 10 } struct A {}"], (value: string) => {
-      const result = createAndCallParser(value)();
-      expect(result).toBeDefined();
-      expect(result).toMatchSnapshot();
-    });
+    const result = createAndCallParser("bitmask foo extends int:16 { A = 10 } struct A {}")();
+    expect(result).toBeDefined();
+    expect(result).toMatchSnapshot();
   });
 
   test("accepts enumerations", () => {
-    each(["enum foo extends int:16 { A = 10 } struct A {}"], (value: string) => {
-      const result = createAndCallParser(value)();
-      expect(result).toBeDefined();
-      expect(result).toMatchSnapshot();
-    });
+    const result = createAndCallParser("enum foo extends int:16 { A = 10 } struct A {}")();
+    expect(result).toBeDefined();
+    expect(result).toMatchSnapshot();
   });
 
   test("accepts == and ||", () => {
@@ -74,13 +72,13 @@ describe("BinrDefinitionImporter", () => {
     expect(result).toMatchSnapshot();
   });
 
-  test("rejects unsupported expression", () => {
-    each(["a++", "a--", "++a", "--a"], (value: string) => {
+  each(["a++", "a--", "++a", "--a"], (value: string) => {
+    test(`rejects unsupported expression (${value})`, () => {
       expect(createAndCallParser(`struct a { int foo[${value}]; }`)).toThrow(/supported/);
     });
   });
-  test("rejects invalid expression", () => {
-    each(["+", "())"], (value: string) => {
+  each(["+", "())"], (value: string) => {
+    test(`rejects invalid expression (${value}`, () => {
       expect(createAndCallParser(`struct a { int foo[${value}]; }`)).toThrow(/parsing/);
     });
   });
