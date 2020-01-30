956416aef193eabba5d6c051ea92e71c84891d75
Guillaume Poussel <gpoussel@gmail.com>
2020-01-30 09:21:22 +0100

    Update some file names

--- packages/definition-importer/lib/sweetscape/sweetscape-definition-importer.ts
+++ packages/definition-importer/lib/sweetscape/sweetscape-definition-importer.ts
@@ -2,7 +2,7 @@ import { CstParser } from "chevrotain";
 
 import { CStylePreprocessor } from "../common/cstyle-preprocessor";
 import { Importer } from "../importer";
-import { SweetscapeDefinitionBuilder } from "./sweetscape-builder";
+import { SweetscapeDefinitionBuilder } from "./sweetscape-definition-builder";
 import { SweetscapeLexer } from "./sweetscape-lexer";
 import { SweetscapeParser } from "./sweetscape-parser";
 import { getVisitor } from "./sweetscape-visitor";
